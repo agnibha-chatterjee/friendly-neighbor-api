6059ee04aa2fd54b728057e9e93bd7ac4679e63d
agnibha-chatterjee <agni.bhaaa@gmail.com>
2020-05-23 15:02:42 +0530

    merged master with https-enable

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ dotenv.config({ path: path.resolve(__dirname, '../config/config.env') });
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import fs from 'fs';
+import https from 'https';
 import connectDB from './db/connectDB';
 import { initializeCloudinary } from './utils/cloudinaryConfig';
 import errorHandler from './middlewares/errorHandler';
@@ -15,13 +17,30 @@ const app = express();
 connectDB();
 initializeCloudinary();
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(
-        morgan(
-            ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'
-        )
-    );
-}
+const privateKey = fs.readFileSync(
+    '/etc/letsencrypt/live/fn.twodee.me/privkey.pem',
+    'utf8'
+);
+const certificate = fs.readFileSync(
+    '/etc/letsencrypt/live/fn.twodee.me/cert.pem',
+    'utf8'
+);
+const ca = fs.readFileSync(
+    '/etc/letsencrypt/live/fn.twodee.me/chain.pem',
+    'utf8'
+);
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca,
+};
+
+app.use(
+    morgan(
+        ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]'
+    )
+);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,7 +53,9 @@ app.use('/api/requests', requestRouter);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () =>
+const httpsServer = https.createServer(credentials, app);
+
+const server = httpsServer.listen(PORT, () =>
     console.log(`Server is  running on port ${PORT}`)
 );
 
